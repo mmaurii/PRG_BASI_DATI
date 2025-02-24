d4512532d27853b542ae5c8d675d69ac50e9621f
mmaurii <119517127+mmaurii@users.noreply.github.com>
2025-02-24 19:57:02 +0100

    added some stored procedures views and triggers

--- script_db_progetto_basi.sql
+++ script_db_progetto_basi.sql
@@ -147,3 +147,167 @@ DESCRIBE CANDIDATURA;
 
 SELECT User, Host FROM mysql.user;
 
+/* DEFINIZIONE DELLE STORED PROCEDURE */
+
+/* creo una procedura per l'Autenticazione sulla piattaforma */
+drop PROCEDURE if exists logIn;
+DELIMITER |
+CREATE PROCEDURE logIn (IN inputMail varchar(255), IN inputPassword varchar(255), OUT isLogIn bool)  
+BEGIN
+	if exists(select mail, password
+				FROM utente
+				WHERE (mail=inputMail) AND (password=inputPassword)) then
+		set isLogIn = true;
+    else
+		set isLogIn = false;
+    end if;
+END;
+|
+DELIMITER ;
+
+/* creo una procedura per la registrazione sulla piattaforma */
+drop PROCEDURE if exists singUp;
+DELIMITER |
+CREATE PROCEDURE singUp (IN inputMail varchar(255),IN inputNickname varchar(255), IN inputPassword varchar(255), IN inputNome varchar(255), IN inputCognome varchar(255), IN inputAnnoN YEAR, IN inputLuogo varchar(255), OUT isSingUp bool) 
+BEGIN
+	if exists(select mail, password
+				FROM utente
+				WHERE (mail=inputMail)) then
+		set isSingUp = false;
+    else
+		insert into utente (mail, nickname, password, nome, cognome, annoN, luogo)
+        values (inputMail, inputNickname, inputPassword, inputNome, inputCognome, inputAnnoN, inputLuogo);
+		set isSingUp = true;
+    end if;
+END;
+|
+DELIMITER ;
+
+/* creo una procedura per l'inserimento delle proprie skills di curriculum */
+drop PROCEDURE if exists setLivelloCompetenza;
+DELIMITER |
+CREATE PROCEDURE setLivelloCompetenza (IN inputLivello int, IN inputCompetenza VARCHAR(255), IN inputMail varchar(255), OUT isSet bool) 
+BEGIN
+	if (inputLivello>=0 and inputLivello <=5) then
+		insert into possiede (mail, competenza, livello)
+        values (inputMail, inputCompetenza, inputLivello);
+		set isSet = true;
+    else
+		set isSet = false;
+    end if;
+END;
+|
+DELIMITER ;
+
+/* creo una procedura per la visualizzazione dei progetti disponibili */
+drop PROCEDURE if exists getProgetti;
+DELIMITER |
+CREATE PROCEDURE getProgetti () 
+BEGIN
+	select *
+    from progetti
+    where stato = 'aperto';
+END;
+|
+DELIMITER ;
+
+/* creo una procedura per il finanziamento di un progetto (aperto). Un utente può finanziare anche il progetto di cui è creatore. */
+drop PROCEDURE if exists finanziaProgetto;
+DELIMITER |
+CREATE PROCEDURE finanziaProgetto(IN imnputMail VARCHAR(255), IN imnputNome VARCHAR(255),IN imnputData DATE, IN imnputImporto int, IN inputCodR VARCHAR(255)) 
+BEGIN
+	if not exists(select * from progetto where (nome = inputNome) and (stato = 'aperto')) then
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'inputNome doesn\'t exists or is not an open project';
+    END IF;
+        
+	insert into finanziamento (mail, nome, data, importo, codR)
+	values (imnputMail, imnputNome, imnputData, imnputImporto, inputCodR);
+END;
+|
+DELIMITER ;
+
+/* creo una procedura per scegliere la reward a valle del finanziamento di un progetto. */
+drop PROCEDURE if exists choseReward;
+DELIMITER |
+CREATE PROCEDURE choseReward(IN imnputMail VARCHAR(255), IN imnputNome VARCHAR(255),IN imnputData DATE, IN inputCodR VARCHAR(255)) 
+BEGIN
+	if (inputCodR is null) then
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'inputCodR cannot be null';
+    END IF;
+
+	if not exists(select * from progetto where nome = inputNome) then
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'inputNome doesn\'t exists';
+    END IF;
+    
+	if not exists(select * from reward where (cod = inputCodR) and (nomeP = inputNome)) then
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'inputCodR doesn\'t exists';
+    END IF;
+    
+	update finanziamento 
+	set codR=inputCodR
+    where (mail = imnputMail) and (nome = imnputNome) and (data = inputData);
+END;
+|
+DELIMITER ;
+
+/* DEFINIZIONE DELLE VIEW */
+
+/* Visualizzare la classifica degli utenti creatori, in base al loro valore di affidabilità. Mostrare solo il nickname dei primi 3 utenti. */
+DROP VIEW if exists viewClassifica;
+CREATE VIEW viewClassifica(mail, affidabilita) AS
+	SELECT mail, affidabilita
+	FROM creatore
+	order by affidabilita desc
+    limit 3;
+
+/* DEFINIZIONE DEI TRIGGER */
+
+/* triggers per aggiornare l’affidabilità di un utente creatore. L’affidabilità viene calcolata come X è la percentuale di progetti creati dall’utente che hanno ottenuto almeno un finanziamento */
+/* condizione: ogni qualvolta un utente crea un progetto*/
+
+DROP TRIGGER if exists aggiornaAffidabilitaOnProgetto;
+DELIMITER |
+CREATE TRIGGER aggiornaAffidabilitaOnProgetto 
+after INSERT ON progetto
+FOR EACH ROW
+BEGIN
+	DECLARE numProgetti INT DEFAULT 0;
+	DECLARE numProgettiFinanziati INT DEFAULT 0;
+	/* Recupero il numero attuale di progetti dell'utente*/
+	select nr_progetti from creatore where mail=new.mailC INTO numProgetti;
+
+	/* Recupero il numero attuale di progetti finanziati*/
+	select count(*) from finanziamento where nome=new.nome INTO numProgettiFinanziati;
+    
+	IF numProgetti > 0 THEN
+		UPDATE creatore SET affidabilita = numProgetti/numProgettiFinanziati WHERE (mail=NEW.mailC);
+	END IF;
+END;
+|
+DELIMITER ;
+
+/* condizione: ogni qualvolta un progetto dell’utente riceve un finanziamento */
+DROP TRIGGER if exists aggiornaAffidabilitaOnFinanziamento;
+DELIMITER |
+CREATE TRIGGER aggiornaAffidabilitaOnFinanziamento 
+after insert on finanziamento 
+FOR EACH ROW
+BEGIN
+	DECLARE numProgetti INT DEFAULT 0;
+	DECLARE numProgettiFinanziati INT DEFAULT 0;
+	/* Recupero il numero attuale di progetti dell'utente*/
+	select c.nr_progetti from creatore c where c.mail in (select p.mail from progetto p where p.nome=new.nome) INTO numProgetti;
+
+	/* Recupero il numero attuale di progetti finanziati*/
+	select count(*) from finanziamento where nome=new.nome INTO numProgettiFinanziati;
+    
+	IF numProgetti > 0 THEN
+		UPDATE creatore SET affidabilita = numProgetti/numProgettiFinanziati WHERE c.mail in (select p.mail from progetto p where p.nome=new.nome);
+	END IF;
+END;
+|
+DELIMITER ;
